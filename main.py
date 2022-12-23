a7a1e9159d60ba1975fefa240646d765aa7c8c07
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2022-12-23 13:32:59 +0200

    1

--- main.py
+++ main.py
@@ -1,8 +1,70 @@
 def task_1_1():
-    pass
+    choise = 'д'
+
+    if choise.upper() == 'Д':
+        print('Вы ввели д!')
+
+
+def task_1_2():
+    mystring = 'Привет я Максим'
+
+    total = 0
+
+    for st in mystring:
+        if st.isspace():
+            total += 1
+
+    print(total)
+
+
+def task_1_3():
+    mystring = 'Я родился в 2004 году'
+
+    total = 0
+
+    for st in mystring:
+        if st.isdigit():
+            total += 1
+
+    print(total)
+
+
+def task_1_4():
+    mystring = 'Привет'
+
+    total = 0
+
+    for st in mystring:
+        if st.islower():
+            total += 1
+
+    print(total)
+
+
+def task_1_5():
+    mystring = 'main.com'
+
+    if mystring.endswith('.com'):
+        print('True')
+    else:
+        print('False')
+
+
+def task_1_6():
+    mystring = 'тимур'
+
+    mystring = mystring.replace('т', 'Т')
+
+    print(mystring)
+
+
+def task_1_7():
+    mystr = '12345'
+
+    mystr = mystr.
+
+    print(mystr)
 
 
 if __name__ == '__main__':
-    task_1_1()
-
-
+    task_1_7()
